afeb5a07b1621bfbed07f2771443523e6f8607ab
soni parth <>
2020-01-08 12:47:44 +0530

    upload files

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,22 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+  getPoints() {
+    return element(by.cssContainingText('div', 'Points')).$('span').getText();
+  }
+
+  getPlus1Button() {
+    return element(by.cssContainingText('button', 'Plus 1'));
+  }
+
+  getResetButton() {
+    return element(by.cssContainingText('button', 'Reset'));
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBPakloFNRSFNOe7r4e8LJg3pyMheB_UiU",
+    authDomain: "angular-cicd-f966b.firebaseapp.com",
+    databaseURL: "https://angular-cicd-f966b.firebaseio.com",
+    projectId: "angular-cicd-f966b",
+    storageBucket: "angular-cicd-f966b.appspot.com",
+    messagingSenderId: "460071492634",
+    appId: "1:460071492634:web:8f130db31bb02e504e85a3",
+    measurementId: "G-P3SG0NZ5F4"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
